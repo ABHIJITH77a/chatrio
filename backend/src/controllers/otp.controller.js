33ef378afcc29949d68f4516bccf54b6ee4ea810
abhijith <jithabhi574@gmail.com>
2025-09-17 14:34:26 +0530

    server-commit6

--- backend/src/controllers/otp.controller.js
+++ backend/src/controllers/otp.controller.js
@@ -60,7 +60,7 @@ export const verifyotp = async (req, res) => {
 
   try {
     const otpData = req.session.otpData;
-    console.log("hi",otpData)
+    console.log("hi",req.session)
  
     if (!otpData) { 
       return res.json({ success: false, message: "No OTP session found. Please request a new OTP" });
